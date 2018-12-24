ea1b425ffe7e388de5a566103f6a82376b7e3da0
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-12-24 11:20:00 -0300

    Adicionando bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelSimplesComponent
   ],
   imports: [
     BrowserModule
