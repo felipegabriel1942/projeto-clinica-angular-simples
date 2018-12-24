fb5444f6de2e4c35ca947e94db8eac6e74ae7cf9
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-12-24 17:36:50 -0300

    Criando barras de navegação superior e lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { BarraLateralComponent } from './barra-navegacao/barra-lateral/barra-lateral.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PainelSimplesComponent
+    PainelSimplesComponent,
+    BarraNavegacaoComponent,
+    BarraLateralComponent
   ],
   imports: [
     BrowserModule
